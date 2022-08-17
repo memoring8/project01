047ef817bb6d3aae21db9b93e18ef72b4deaae0c
memoring8 <memo091577@gmail.com>
2022-08-17 19:22:21 +0900

    signup/signin

---
+++ sign/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/signup', function(req,res){
+    res.render('signUp');
+});
+
+app.get('/signin', function(req,res){
+    res.render('signIn');
+});
+
+app.listen(port, ()=>{
+    console.log('server port :', port)
+})
